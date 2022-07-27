416a613ec3b32848d22e58293b2ce7866a3bade3
codewithsathya <codewithsathya@gmail.com>
2022-07-27 14:10:34 +0530

    Added run configuration

---
+++ src/main/java/com/codewithsathya/algorithms/graphs/BreadthFirstSearchAdjacencyListIterative.java
@@ -0,0 +1,11 @@
+package com.codewithsathya.algorithms.graphs;
+
+import java.util.Scanner;
+
+public class BreadthFirstSearchAdjacencyListIterative {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println("Hello world");
+    }
+}
